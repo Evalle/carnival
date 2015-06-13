240202e1641609474f45587579d8295861623772
evalle <shmarnev@gmail.com>
2015-06-13 21:38:27 +0200

    fix some issues with output

--- dockerauto.sh
+++ dockerauto.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-# v 0.03
+# v 0.05
 # =======
-# There are two important things:
-# 1) you need to be root to run this script;
-# 2) this script is useful only for basic Docker functionality testing on your system. 
+# There are four important things:
+# 1) you need to have root privilegies to run this script;
+# 2) this script only testing basic Docker functionality on your system;
+# 3) all your results in results.txt file;
+# 4) have a lot of fun!
 
 ERRORS='0'
 
-
-# fucntion that checking status of your command
+/# fucntion that checking status of your command
 function check {
     if [ $? -eq 0 ]; then
         echo "Test Passed"
@@ -18,82 +19,83 @@ function check {
     fi
 }
 
-echo "dockerat.sh is now testing Docker on your system, you can find all results in '$1' file. Please, be patient..."
+echo "dockerat.sh is now testing Docker on your system, you can find your results in 'results.txt' file. Please, be patient..."
 sleep 1 
-echo "Docker testing on '$HOSTNAME'" > $1
-echo "" >> $1
+echo "Docker testing on '$HOSTNAME'" > results.txt
+echo "" >> results.txt
 systemctl stop docker
 check
 
 sleep 2
-echo "" >> $1
-echo "$HOSTNAME:~ # systemctl status docker" >> $1
-systemctl status docker >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # systemctl status docker" >> results.txt
+systemctl status docker >> results.txt
 check
 
-echo "" >> $1
-echo "$HOSTNAME:~ # systemctl start docker" >> $1
-systemctl start docker >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # systemctl start docker" >> results.txt
+systemctl start docker >> results.txt
 check
 
 sleep 2
-echo "" >> $1
-echo "$HOSTNAME:~ # systemctl status docker" >> $1
-systemctl status docker >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # systemctl status docker" >> results.txt
+systemctl status docker >> results.txt
 check
 
-echo "" >> $1
-echo "$HOSTNAME:~ # ip a" >> $1
-# We need "ip a s | grep -i docker"  here to find that we have docker newtork interface here >> TODO
-ip a >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # ip a" >> results.txt
+# We probably need to use "ip a s | grep -i docker" here to find out that we have docker network interface >> TODO
+ip a >> results.txt
 check 
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker ps" >> $1
-docker ps >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker ps" >> results.txt
+docker ps >> results.txt
 check
+sleep 1
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker run ubuntu uname -r" >> $1
-docker run ubuntu uname -r &>> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker run ubuntu uname -r" >> results.txt
+docker run ubuntu uname -r &>> results.txt
 check 
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker run ubuntu echo 'Hello world!'" >> $1
-docker run ubuntu echo 'Hello world!' >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker run ubuntu echo 'Hello world!'" >> results.txt
+docker run ubuntu echo 'Hello world!' >> results.txt
 check 
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker run ubuntu df -h " >> $1
-docker run ubuntu df -h >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker run ubuntu df -h " >> results.txt
+docker run ubuntu df -h >> results.txt
 check 
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker run ubuntu mount" >> $1
-docker run ubuntu mount >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker run ubuntu mount" >> results.txt
+docker run ubuntu mount >> results.txt
 check
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker ps -a" >> $1
-docker ps -a >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker ps -a" >> results.txt
+docker ps -a >> results.txt
 check
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker images " >> $1
-docker images >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker images " >> results.txt
+docker images >> results.txt
 check
 
-echo "" >> $1
-echo "$HOSTNAME:~ # docker info" >> $1
-docker info >> $1
+echo "" >> results.txt
+echo "$HOSTNAME:~ # docker info" >> results.txt
+docker info >> results.txt
 check
 
 echo "Do you wish to destory all Docker images and containers on your system? (Please choose  1 - for Yes, and 2 - for No)"
 select yn in "Yes" "No"; do
     case $yn in 
-        Yes ) docker rm -f $(docker ps -a -q) && docker rmi $(docker images -q) > /dev/null; break;;
+        Yes ) docker rm -f $(docker ps -a -q) && docker rmi $(docker images -q) &> /dev/null; break;;
         No  ) exit;;
     esac
 done
 echo "" 
-echo "Done! Please check '$1' files for results "
+echo "Done! Please check 'results.txt' for results "
