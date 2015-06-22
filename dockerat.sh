542c6e3444b33ada0663935c6543415138e19c39
evalle <shmarnev@gmail.com>
2015-06-22 09:22:11 +0200

    add some comments to dockerat.sh file

--- dockerat.sh
+++ dockerat.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # v 1.0
 # =======
-# Docker auto testing
-#########################
-# There are four important things:
+# Docker auto testing script aka docke_rat
+############################################
+# There are five important things:
 # 1) you need to have root privilegies to run this script;
-# 2) this script is only testing basic Docker functionality on your system;
-# 3) you can find all your results in the log file (see $LOG variable);
-# 4) have a lot of fun!
+# 2) this scripts accepts one input argument value - output of the `docker --version` command. (e.g. sudo ./dockerat.sh "Docker version 1.6.2, build 7c8fca2")
+# 3) this script is only testing basic Docker functionality on your system;
+# 4) you can find all your results in the log file (see $LOG variable);
+# 5) have a lot of fun!
 
 ERRORS=0
 
