4c96811ab4ba108fe55c2d86f6381f9b131a2255
evalle <eshmarnev@suse.com>
2015-06-23 15:53:54 +0200

    delete columns from main .sh file

--- dockerat.sh
+++ dockerat.sh
@@ -143,7 +143,7 @@ echo ""
 echo "Do you wish to destory all Docker images and containers on your system? (Please choose  1 - for Yes, and 2 - for No)"
 select yn in "Yes" "No"; do
     case $yn in 
-        Yes ) echo ""; echo "Deleting your containers: "; echo ""; docker rm -f "$(docker ps -a -q)" && docker rmi "$(docker images -q)" &> /dev/null; break;;
+        Yes ) echo ""; echo "Deleting your containers: "; echo ""; docker rm -f $(docker ps -a -q) && docker rmi $(docker images -q) &> /dev/null; break;;
         No  ) exit;;
     esac
 done
